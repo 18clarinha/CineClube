ee91b3abb9d80e26c379409bbc41180da4cc6088
18clarinha <clara.guil@escola.pr.gov.br>
2025-06-04 21:55:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mostraGaleria() {
+    var gallery = document.getElementById("gallery");
+    gallery.classList.add("visible");
+}
+
+function escondeGaleria() {
+    var gallery = document.getElementById("gallery");
+    gallery.classList.remove("visible");
+}
